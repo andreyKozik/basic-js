3c8db7bc821ed6dd02ea41e0c20133c87a29932b
Andrei Kozik <13pgs1.kozik.a@pdu.by>
2020-02-19 01:33:45 +0300

    complite dream-team

--- src/dream-team.js
+++ src/dream-team.js
@@ -1,4 +1,11 @@
-module.exports = function createDreamTeam(/* members */) {
-  throw 'Not implemented';
-  // remove line with error and write your code here
+module.exports = function createDreamTeam(members) {
+  if (!Array.isArray(members)) return false
+  let codName = '';
+  members.map(a => {
+    if(typeof a === "string") {
+      a = a.trim().toUpperCase();;
+      codName = codName + a.slice(0, 1);
+    }
+  })
+  return codName.split('').sort().join('')
 };
