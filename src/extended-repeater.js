213990e999478a213faf93e2b66ec4ba11fae56a
Andrei Kozik <13pgs1.kozik.a@pdu.by>
2020-02-19 14:06:58 +0300

    complite extended-repeater

--- src/extended-repeater.js
+++ src/extended-repeater.js
@@ -1,5 +1,20 @@
-module.exports = function repeater(/* str, options */) {
-    throw 'Not implemented';
-    // remove line with error and write your code here
+module.exports = function repeater(str, o) {
+  if (o.separator === undefined) o.separator = "+";
+  if (o.addition === undefined) o.addition = "";
+  let result = "";
+  result += str;
+  while (o.additionRepeatTimes > 1) {
+    result += o.addition;
+    result += o.additionSeparator;
+    o.additionRepeatTimes--;
+  }
+  result += o.addition;
+  let clone = result;
+  while (o.repeatTimes > 1) {
+    result += o.separator;
+    result += clone;
+    o.repeatTimes--;
+  }
+  //   result += clone;
+  return result;
 };
-  
