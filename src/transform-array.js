66c5eb28a20b555903737846a65cc5e3782cbbab
Andrei Kozik <13pgs1.kozik.a@pdu.by>
2020-02-19 22:45:22 +0300

    complite tasks

--- src/transform-array.js
+++ src/transform-array.js
@@ -1,4 +1,19 @@
-module.exports = function transform(/* arr */) {
-    throw 'Not implemented';
-    // remove line with error and write your code here
+module.exports = function transform(arr) {
+  let array = [];
+  if (!Array.isArray(arr)) throw new Error();
+
+  for (let i = 0; i < arr.length; ++i) {
+    if (arr[i] === "--discard-next") {
+      if (i < arr.length - 1) i++;
+    } else if (arr[i] === "--discard-prev") {
+      if (array.length > 0) array.pop();
+    } else if (arr[i] === "--double-next") {
+      if (i < arr.length - 1) array.push(arr[i + 1]);
+    } else if (arr[i] === "--double-prev") {
+      if (i > 0) array.push(arr[i - 1]);
+    } else {
+      array.push(arr[i]);
+    }
+  }
+  return array;
 };
--- src/what-season.js
+++ src/what-season.js
@@ -1,4 +1,23 @@
-module.exports = function getSeason(/* date */) {
-  throw 'Not implemented';
-  // remove line with error and write your code here
+module.exports = function getSeason(date) {
+  if (date === undefined) return "Unable to determine the time of year!";
+  if (Object.prototype.toString.call(date) !== "[object Date]") throw new Error();
+
+  const season = {
+    "11": "winter",
+    "0": "winter",
+    "1": "winter",
+    "2": "spring",
+    "3": "spring",
+    "4": "spring",
+    "5": "summer",
+    "6": "summer",
+    "7": "summer",
+    "8": "autumn",
+    "9": "autumn",
+    "10": "autumn"
+  };
+  const numberMonth = date.getMonth();
+  for (key in season) {
+    if (numberMonth === +key) return season[key];
+  }
 };
